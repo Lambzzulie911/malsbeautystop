b6e9aa7e86f3b5dc62c6b0cd7c367de2e691295c
Lambzzulie911 <st10471191@rcconnect.edu.za>
2025-06-20 04:28:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -44,14 +44,7 @@ function validateForm(formId) {
 validateForm("contactForm");
 validateForm("enquiryForm");
 
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
+
 
 
 
