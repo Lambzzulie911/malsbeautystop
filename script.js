f2079f356af7b37b32ee0d6460e1b992c2199805
Lambzzulie911 <st10471191@rcconnect.edu.za>
2025-06-24 00:53:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -71,28 +71,3 @@ validateForm("contactForm");
 validateForm("enquiryForm");
 
 
-// Show/hide the button when scrolling
-window.onscroll = function () {
-  const btn = document.getElementById("backToTop");
-  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-    btn.style.display = "block";
-  } else {
-    btn.style.display = "none";
-  }
-};
-
-// Scroll to top when button is clicked
-document.getElementById("backToTop").addEventListener("click", function () {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  });
-});
-
-
-
-
-
-
-
-
